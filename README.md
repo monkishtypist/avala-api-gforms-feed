9f08762032718fa0c8fd81f4d9df0001a686bef1
Tim Spinks <tim@ninthlink.com>
2017-02-02 10:54:16 -0800

    Updated readme instructions and notes, license and author details corrected.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Gravity Forms - Avala API Add-On
-A Gravity Forms add-on to connect GForms submits to Avala Aimbase CRM
+# Gravity Forms to Avala API Add-On
+
+A custom Gravity Forms add-on to submit Gravity Forms entries to Avala Aimbase CRM
+
+## Installation ##
+
+1. Download plugin Zip file
+2. Unpack files to `avala-api-gforms-feed` directory in your WordPress plugins directory
+3. Activate plugin
+4. Go to Gravity Forms settings > Avala API and enter your API information
+5. Go to Form Settings and create a new connection
+
+## Requirements ##
+
+This plugin requires a subscription to Aimbase CRM that supports API POSTs, as well as the Gravity Forms plugin for WordPress.
+
+## Notes ##
+
+This plugin is not officially supported by Avala. They, however, do not have any such plugin of their own. So use this!
--- avala-api-gforms-feed.php
+++ avala-api-gforms-feed.php
@@ -1,29 +1,13 @@
 <?php
 /*
 Plugin Name: Gravity Forms - Avala API Add-On
-Plugin URI: http://www.ninthlink.com
-Description: A Gravity Forms add-on to connect GForms submits to Avala Aimbase CRM
+Plugin URI: https://github.com/monkishtypist/avala-api-gforms-feed
+Description: A Gravity Forms add-on to submit Gravity Form entries to Avala Aimbase CRM
 Version: 1.0
-Author: TimS @ Ninthlink
-Author URI: http://www.ninthlink.com
+Author: Tim Spinks
+Author URI: http://www.monkishtypist.com
 Documentation: http://www.gravityhelp.com/documentation/page/GFAddOn
-
-------------------------------------------------------------------------
-Copyright 2014 Ninthlink, Inc.
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+License: GPL-2.0
 */
 
 //exit if accessed directly
